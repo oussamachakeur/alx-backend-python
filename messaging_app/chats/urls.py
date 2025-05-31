78ef0fdc756b891e08eb289ba27883d72d48d6b7
chakeur oussama <chakeuroussama@gmail.com>
2025-05-31 21:19:32 +0800

    django setip

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,21 +1,11 @@
-from django.urls import path
-from .views import (
-    UserListView, UserDetailView,
-    ConversationListCreateView, ConversationDetailView,
-    MessageListCreateView, MessageDetailView,UserCreateView
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    # User endpoints
-    path('register/', UserCreateView.as_view(), name='user-register'),
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-
-    # Conversation endpoints
-    path('conversations/', ConversationListCreateView.as_view(), name='conversation-list'),
-    path('conversations/<int:pk>/', ConversationDetailView.as_view(), name='conversation-detail'),
-
-    # Message endpoints
-    path('messages/', MessageListCreateView.as_view(), name='message-list'),
-    path('messages/<int:pk>/', MessageDetailView.as_view(), name='message-detail'),
+    path('', include(router.urls)),
 ]
