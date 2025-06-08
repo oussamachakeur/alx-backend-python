9630572d1726b85aa65da30f0f4872c5896a523f
chakeur oussama <chakeuroussama@gmail.com>
2025-06-08 18:22:25 +0800

    nice

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -4,7 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10  # default page size
+    page_size = 20  # default page size
     page_size_query_param = 'page_size'
     max_page_size = 100
 
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'chats.pagination.CustomPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
 
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
